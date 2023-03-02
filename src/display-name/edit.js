43f5a7eda69b7319e67f318219c9cd391304b5cb
Ryan Dayalo <30278598+ryandayalo@users.noreply.github.com>
2023-03-02 23:38:14 +0800

    initial commit

---
+++ src/display-name/edit.js
@@ -0,0 +1,51 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { TextControl } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import ServerSideRender from '@wordpress/server-side-render';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+import { name } from './block.json';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( { attributes, setAttributes }) {
+
+	const { authorName } = attributes
+
+	return (
+		<div { ...useBlockProps() }>
+
+			<p>What is your name?</p>
+			<TextControl
+				label="Insert your name here"
+				value={authorName}
+				onChange={ (newAuthorName) => setAttributes({ authorName: newAuthorName }) }
+			/>
+		</div>
+	);
+}
---
+++ src/display-name/render.php
@@ -0,0 +1,5 @@
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php
+		echo 'Hello, My name is ' .$attributes['authorName'];
+	?>
+</p>
